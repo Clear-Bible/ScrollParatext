800094a7c236b475004d22ee38b04a3aba79660f
GeoDirk <geodirk@gmail.com>
2023-05-17 11:26:45 -0400

    Update SantaFeFocusMessageHandler.cs

--- ScrollParatext.Messages/SantaFeFocusMessageHandler.cs
+++ ScrollParatext.Messages/SantaFeFocusMessageHandler.cs
@@ -1,4 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------------
+ 
+Code copied from the MIT licensed project here: https://github.com/sillsdev/libpalaso 
+ 
 #region // Copyright (c) 2007, SIL International. All Rights Reserved.
 // <copyright from='2007' to='2007' company='SIL International'>
 //		Copyright (c) 2007, SIL International. All Rights Reserved.   
